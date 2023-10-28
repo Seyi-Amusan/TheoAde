08a60ffc59f2668c480a18848e472106f607d460
Seyi Amusan <Seyijoshua14@gmail.com>
2023-10-28 20:26:24 +0100

    Pushing Code to Github Repo

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,35 @@ module.exports = {
   './public/home.html'
 ],
   theme: {
+    screens : {
+      'blg': '930px',
+    },
+
     extend: {
+      colors: {
+        'text': '#130e01',
+        'background': '#fffaeb',
+        'primary': {
+          50: '#fff3e5',
+          100: '#ffe6cc',
+          200: '#ffce99',
+          300: '#ffb566',
+          400: '#ff9c33',
+          500: '#ff8400',
+          600: '#cc6900',
+          700: '#994f00',
+          800: '#663500',
+          900: '#331a00',
+          950: '#1a0d00',
+        },
+        'secondary': '#fff5d6',
+        'accent': '#cf4307',
+      },
+       
+       
       fontFamily: {
-        body: ['Nunito']
-      }
+        'body': ['Nunito']
+      },
     },
   },
   plugins: [],
