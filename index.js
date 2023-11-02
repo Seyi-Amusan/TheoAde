929bffbfc89f634fe2a88d83518a33d65785a33b
Seyi Amusan <Seyijoshua14@gmail.com>
2023-11-02 12:45:44 +0100

    Home page completed, responiveness and navigation solved

--- index.js
+++ index.js
@@ -3,37 +3,31 @@ const menus = document.querySelector('#menus');
 const viewportWidth = window.innerWidth;
 
 const displayNavFunc = () => {
-    menuBar.addEventListener('click', () => { //when the nav icon is clicked
+    menuBar.addEventListener('click', () => {
         if (menuBar.classList.contains('openMenuBar') && menus.classList.contains('hidden')) {
             menus.classList.remove('hidden'); //makes the nav visible
             menus.classList.add('grid'); //displays the nav as a grid
             menuBar.src = "./public/icons/closeMenu.svg"; //changes the nav icon
             menuBar.classList.remove('openMenuBar'); 
-            menuBar.classList.add('closeMenuBar')
+            menuBar.classList.add('closeMenuBar');
+            console.log('true');
+        } else if (menuBar.classList.contains('closeMenuBar') && menus.classList.contains('grid')) {
+                menus.classList.remove('grid'); //makes the nav visible
+                menus.classList.add('hidden'); //displays the nav as a grid
+                menuBar.src = "./public/icons/openMenu.svg"; //changes the nav icon
+                menuBar.classList.remove('closeMenuBar'); 
+                menuBar.classList.add('openMenuBar');
+                console.log('false');
         } 
-        return; 
+        return;
     });
 }
 
-const removeNavFunc = () => {
-    // menuBar.addEventListener('click', () => { 
-    //     if (menuBar.classList.contains('closeMenuBar') && menus.classList.contains('grid')) {
-    //         menus.classList.remove('grid'); //makes the nav visible
-    //         menus.classList.add('hidden'); //displays the nav as a grid
-    //         menuBar.src = "./public/icons/openMenu.svg"; //changes the nav icon
-    //         menuBar.classList.remove('closeMenuBar'); 
-    //         menuBar.classList.add('closeMenuBar')
-    //         console.log('condirion is true');
-    //     } 
-    //     return; 
-    // });
-}
 
 const navFunc = () => {
     if (viewportWidth < 1020) {
         menus.classList.add('nav-mobile');
     }
-    menuBar.classList.add('hidden');
     return;
 }
 
@@ -41,7 +35,6 @@ const navFunc = () => {
 const initApp = () => {
     navFunc();
     displayNavFunc();
-    removeNavFunc();
 }
 
 
