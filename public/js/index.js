514f313b2dab6b69b50f571ecdea1b9a53ab367e
Seyi Amusan <Seyijoshua14@gmail.com>
2023-12-14 18:31:01 +0100

    Improved homepage design

--- public/js/index.js
+++ public/js/index.js
@@ -6,31 +6,7 @@ const menus = document.querySelector('#menus');
 const viewportWidth = window.innerWidth;
 const viewportHeight = window.innerHeight;
 const scroll = window.scrollY;
-// const nav = document.createElement('nav')
-// nav.classList.add('z-30', 'grid',  'grid-cols-1', 'grid-rows-1', 'justify-between', 'border-b', 'border-b-gray-400', 'lg:p-5', 'lg:flex', 'lg:justify-end', 'lg:m-7')
-// console.log(nav);
-
-// nav.append(navChild)
-
-// import {navChild} from './components.js';
-
-// header.insertBefore(nav, div);
-
-// console.log(typeof nav);
-// body.append(nav);
-// console.log(body.firstChild);
-// console.log(x);
-// console.log(nav);
-
-// body.firstChild = nav
-
-// console.log(scroll);
-// console.log(viewportHeight);
-// console.log(document.querySelector('.header'));
-
-// if (scroll > viewportHeight) {
-//     console.log('good');
-// }
+const nav = document.querySelector('#nav')
 
 const displayNavFunc = () => {
     menuBar.addEventListener('click', () => {
@@ -40,15 +16,13 @@ const displayNavFunc = () => {
             menuBar.src = "./public/icons/closeMenu.svg"; //changes the nav icon
             menuBar.classList.remove('openMenuBar'); 
             menuBar.classList.add('closeMenuBar');
-            console.log('true');
         } else if (menuBar.classList.contains('closeMenuBar') && menus.classList.contains('grid')) {
                 menus.classList.remove('grid'); //makes the nav invisible
                 menus.classList.add('hidden'); //displays the nav as a hidden
                 menuBar.src = "./public/icons/openMenu.svg"; //changes the nav icon
                 menuBar.classList.remove('closeMenuBar'); 
                 menuBar.classList.add('openMenuBar');
-                console.log('false');
-        } 
+        }
         return;
     });
 }
